c6b9c93c14a1692997d6e6810673ae6f53769648
Issa Sangare <issabmsangare@hotmail.com>
2020-04-08 16:34:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,13 @@ d.collect do |i|
 i.reverse
 
 end
+ reverse_word = []
+string.split 
+counter = 0
+while counter < string.split.length do 
+string.split[counter].reverse 
+reverse_word<<string.split[counter].reverse 
+counter+=1
+end 
+reverse_word
+end
