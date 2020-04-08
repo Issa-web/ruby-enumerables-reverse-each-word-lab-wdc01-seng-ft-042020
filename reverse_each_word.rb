1ce7e5b6bab7068ffefe51ca4ddb829c34fa9fe0
Issa Sangare <issabmsangare@hotmail.com>
2020-04-08 16:13:28 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(string)
+  reverse_word = []
+string.split 
+counter = 0
+while counter < string.split.length do 
+string.split[counter].reverse 
+reverse_word<<string.split[counter].reverse 
+counter+=1
+end 
+reverse_word
+end
